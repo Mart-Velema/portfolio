38776e9a4ae7eed67008651e3002715017a9669c
mart-Velema <mart.velema@student.nhlstenden.com>
2023-11-30 00:39:48 +0100

    Started working on FLOOR, error messages for incorrect settings have been implemented

--- floor.php
+++ floor.php
@@ -12,9 +12,84 @@
         include "components/header.php"
     ?>
     <main>
-        <form action="#" method="post">
-
+        <?php
+            if(empty($_GET['scene']))
+            {
+                echo '<p style="background-color: white; color: red; grid-column: 2 / 3; height: 2rem;">No scene selected! Please select a scene.</p>';
+                $_GET['scene'] ='';
+            };
+            if(empty($_GET['directory']))
+            {
+                echo '<p style="background-color: white; color: red; grid-column: 2 / 3; height: 2rem;">No working directory selected! Please select directory where images are stored.</p>';
+                $_GET['directory'] ='';
+            };
+        ?>
+        <form method="get" style="grid-column: 1 / 2; height:min-content; width: 80%;">
+            <h3>settings</h3>
+            <label for="scene">Select scene mode</label>
+            <select name="scene" id="scene">
+                <option value="1">Single-image scenes</option>
+                <option value="2">Double-image scenes</option>
+            </select>
+            <label for="directory">Set directory</label>
+            <input type="text" name="directory" id="directory">
+            <button type="submit">Set</button>
         </form>
+        <form method="post">
+            <?php
+                switch($_GET['scene'])
+                {
+                    case 1:
+                        echo
+                        '<label for="img">Filename image</label>' .
+                        '<input type="text" name="img" id="img">';
+                        break;
+                    case 2:
+                        echo
+                        '<label for="imgL">Filename left image</label>' .
+                        '<input type="text" name="imgL" id="imgL">' .
+                        '<label for="imgR">Filename right image</label>' .
+                        '<input type="text" name="imgR" id="imgR">';
+                        break;
+                    default:
+                        echo '<p style="background-color: white; color: red;">Incorrect scene settings! Please select a correct scene type</p>';
+                        break;
+                };
+            ?>
+            <!-- <?php
+                $test =
+                [
+                    [
+                        "img" =>
+                        [
+                            "img" => "k_kit02_egao",
+                            "talking" => 1
+                        ],
+                        "dialogue" => "Hello, World!",
+                        "item" => "k_kit02_eago"
+                    ],
+                    [
+                        "img" =>
+                        [
+                            "img" => "k_kit02_egao",
+                            "rotate" => 1
+                        ],
+                        "dialogue" => "Test World!"
+                    ],
+                    [
+                        "img" =>
+                        [
+                            "img" => "k_kit02_egao",
+                        ],
+                        "dialogue" => "Goodbye, World!"
+                    ]
+                ];
+                var_dump($test);
+                json_encode($test, JSON_PRETTY_PRINT);
+                var_dump($test);
+            ?> -->
+        </form>
+        <p></p>
     </main>
     <?php
         include "components/footer.php"
