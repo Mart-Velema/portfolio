f4eb566276a1643555b428a5d1922557dd37ea63
mart-Velema <mart.velema@student.nhlstenden.com>
2023-11-30 23:39:51 +0100

    Added decoder for simgle-page scenes for FLOOR

--- floor.php
+++ floor.php
@@ -1,3 +1,12 @@
+<?php
+    /*
+    * Filename      : floor.php
+    * Created       : 28-11-2023
+    * Description   : FLOOR level editor for SCUFF
+    * Programmer    : Mart Velema
+    */
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,18 +21,6 @@
         include "components/header.php"
     ?>
     <main>
-        <?php
-            if(empty($_GET['scene']))
-            {
-                echo '<p class="warning">No scene selected! Please select a scene.</p>';
-                $_GET['scene'] ='';
-            };
-            if(empty($_GET['directory']))
-            {
-                echo '<p class="warning">No working directory selected! Please select directory where images are stored.</p>';
-                $_GET['directory'] ='';
-            };
-        ?>
         <form method="get" style="grid-column: 1 / 2; height:min-content; width: 100%;">
             <h3>settings</h3>
             <label for="scene">Select scene mode</label>
@@ -31,74 +28,111 @@
                 <option value="1">Single-image scenes</option>
                 <option value="2">Double-image scenes</option>
             </select>
-            <label for="directory">Set directory</label>
-            <input type="text" name="directory" id="directory">
+            <label for="dir">Set directory</label>
+            <input type="text" name="dir" id="dir" required>
             <button type="submit">Set</button>
         </form>
-        <form method="post">
-            <?php
-                switch($_GET['scene'])
+        <?php
+            if(empty($_GET))
+            {
+                echo '<p class="warning">Data for directory and scene is unset! please set this requierd data before continiuing</p>';
+                $dir = '';
+                $scene = '';
+            }
+            else
+            {
+                $dir = $_GET['dir'];
+                $scene = $_GET['scene'];
+            };
+            if($_SERVER['REQUEST_METHOD'] == "POST")
+            {
+                switch($_POST['submit'])
+                {
+                    case "generate":
+                        if(empty($_SESSION['form-data']))
+                        {
+                            $_SESSION['form-data'] = '';
+                        };
+                        if($scene == 1)
+                        {
+                            $page = count($_SESSION['form-data']);
+                            $_SESSION['form-data'][$page] = 
+                            [
+                                "img" =>
+                                [
+                                    "img"       => filter_input(INPUT_POST, "img"),
+                                    "talking"   => filter_input(INPUT_POST, "img-talking"),
+                                    "rotate"    => filter_input(INPUT_POST, "img-rotate")
+                                ],
+                                "item"           => filter_input(INPUT_POST, "item"),
+                                "dialogue"       => filter_input(INPUT_POST, "dialogue")
+                            ];
+                            var_dump($_SESSION['form-data']);
+                        }
+                        else
+                        {
+
+                        };
+                        break;
+                    case "done":
+                        break;
+                    case "reset":
+                        $_SESSION['form-data'] = '';
+                        break;
+                };
+            };
+            $pageData =
+            [
+                "scene" => $scene,
+                "dir"   => $dir
+            ];
+            $pageData = http_build_query($pageData);
+            echo '<form action="floor.php?' . $pageData . '" method="post">';
+                switch($scene)
                 {
                     case 1:
                         echo
                         '<label for="img">Filename image</label>' .
                         '<input type="text" name="img" id="img">' . 
                         '<label for="img-talking">Talking</label>' .
-                        '<input type="checkbox" name="img-talking" id="talking">' .
+                        '<input type="checkbox" name="img-talking" id="img-talking" value="1">' .
                         '<label for="img-rotate">Rotate</label>' .
-                        '<input type="checkbox" name="img-rotate id="rotate">' .
-                        '<label for="item">Filename item</label>' .
-                        '<input type="text" name="item" id="item">' .
-                        '<label for="dialogue">Dialogue</label>' .
-                        '<textarea name="dialogue" id="dialogue"></textarea>' .
-                        '<button type="submit">Generate</button>';
+                        '<input type="checkbox" name="img-rotate" id="img-rotate" value="1">';
                         break;
                     case 2:
                         echo
                         '<label for="imgL">Filename left image</label>' .
                         '<input type="text" name="imgL" id="imgL">' .
+                        '<label for="imgL-talking">Talking</label>' .
+                        '<input type="checkbox" name="imgL-talking" id="imgL-talking" value="1">' .
+                        '<label for="imgL-rotate">Rotate</label>' .
+                        '<input type="checkbox" name="imgL-rotate id="imgL-rotate" value="1">' .
                         '<label for="imgR">Filename right image</label>' .
-                        '<input type="text" name="imgR" id="imgR">';
+                        '<input type="text" name="imgR" id="imgR">' .
+                        '<label for="imgR-talking">Talking</label>' .
+                        '<input type="checkbox" name="imgR-talking" id="imgR-talking" value="1">' .
+                        '<label for="imgR-rotate">Rotate</label>' .
+                        '<input type="checkbox" name="imgR-rotate id="imgR-rotate" value="1">';
                         break;
                     default:
                         echo '<p class="warning">Incorrect scene settings! Please select a correct scene type</p>';
                         break;
                 };
             ?>
-            <!-- <?php
-                $test =
-                [
-                    [
-                        "img" =>
-                        [
-                            "img" => "k_kit02_egao",
-                            "talking" => 1
-                        ],
-                        "dialogue" => "Hello, World!",
-                        "item" => "k_kit02_eago"
-                    ],
-                    [
-                        "img" =>
-                        [
-                            "img" => "k_kit02_egao",
-                            "rotate" => 1
-                        ],
-                        "dialogue" => "Test World!"
-                    ],
-                    [
-                        "img" =>
-                        [
-                            "img" => "k_kit02_egao",
-                        ],
-                        "dialogue" => "Goodbye, World!"
-                    ]
-                ];
-                var_dump($test);
-                json_encode($test, JSON_PRETTY_PRINT);
-                var_dump($test);
-            ?> -->
+            <label for="item">Filename item</label>
+            <input type="text" name="item" id="item">
+            <label for="dialogue">Dialogue</label>
+            <textarea name="dialogue" id="dialogue"></textarea>
+            <div>
+                <button type="submit" name="submit" value="generate">Generate</button>
+                <button type="submit" name="submit" value="done">Done</button>
+                <button type="submit" name="submit" value="reset">Reset</button>
+            </div>
+            <?php
+                //echo '<a href=""></a>';
+            ?>
+            <p>Welcome to the FLOOR Level-Editor. In here, you can make levels for the SCUFF Engine.<br>To use this engine, you first need to set up the correct directory and scene. You can do so in the menu on the top left side of this page. Set up the mode for single or double image scene, and enter a directory where the imgaes are saved.<br><br>For build-in assets, enter "GR-Portrait"<br><br>Once all the input fields are filled in, press "Generate" to complete a page.<br>Repeat untill you have made all of the pages, select the "done" checkbox to get the .json file that contains all the data for SCUFF to use.</p>
         </form>
-        <p></p>
     </main>
     <?php
         include "components/footer.php"
