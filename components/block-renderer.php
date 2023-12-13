19f33fa21ea4b80ed53c0b3b48ee958541816527
mart-Velema <mart.velema@student.nhlstenden.com>
2023-12-13 16:34:28 +0100

    Some more optimisations to the block renderer to the external redirect manager to be more in line with the rest of the block renderer

--- components/block-renderer.php
+++ components/block-renderer.php
@@ -8,13 +8,10 @@
     $json = json_decode(file_get_contents('data/' . $id . '.json') , true);
     foreach($json as $name => $projects)
     {
-        if(empty($projects['external']))
-        {
-            $projects['external'] = 'self';
-        };
+        $target = array_key_exists('external', $projects) ? $projects['external'] : 'self';
         $img = array_key_exists('img', $projects) ? '<img src="img/' . $projects['img'] . '.png" alt="' . $projects['img'] . '">' : '';
         $id = str_replace(' ', '-', $projects['name']);
-        $title = isset($projects['link']) ? '<a href="' . $projects['link'] . '" target="_' . $projects['external'] . '"><h2>' . $projects['name']. '</h2></a>' : '<h2>' . $projects['name'] . '</h2>';
+        $title = isset($projects['link']) ? '<a href="' . $projects['link'] . '" target="_' . $target . '"><h2>' . $projects['name']. '</h2></a>' : '<h2>' . $projects['name'] . '</h2>';
         echo
         '<div class="block" id=' . $id . '>' . 
             '' . $title . '' .
